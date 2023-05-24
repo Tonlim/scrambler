ba64c16897d9500e615ff28693dd11871b3cee08
Nick <aelterman.nick@gmail.com>
2023-05-24 18:33:32 +0200

    add a few unit tests for translate

--- src/translate.rs
+++ src/translate.rs
@@ -9,3 +9,28 @@ pub fn translate_word(word: &str) -> Result<String, String> {
         ),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::translate_word;
+
+    #[test]
+    fn translate_single_word() {
+        let result = translate_word("word");
+        assert!(result.is_ok());
+        assert_eq!(result.ok().unwrap(), "Translation of \"word\".");
+    }
+
+    #[test]
+    fn translate_empty() {
+        let result = translate_word("");
+        assert!(result.is_ok());
+        assert_eq!(result.ok().unwrap(), "");
+    }
+
+    #[test]
+    fn cant_translate_two_words() {
+        let result = translate_word("word another one");
+        assert!(result.is_err());
+    }
+}
