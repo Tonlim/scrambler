11696b6c0c3c27806bd5aa1fd086694b08782d84
Nick <aelterman.nick@gmail.com>
2023-06-04 01:09:56 +0200

    cleaning up constants

--- src/scrambler/storage.rs
+++ src/scrambler/storage.rs
@@ -1,3 +1,4 @@
+use const_format::concatcp;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Display;
@@ -7,17 +8,20 @@ use std::fs::OpenOptions;
 use std::io::BufReader;
 use std::io::BufWriter;
 
+const DATA_DIRECTORY: &str = "scrambler_data";
+const TRANSLATED_WORDS: &str = "translated_words.json";
+const TRANSLATED_WORDS_FILE: &str = concatcp!(DATA_DIRECTORY, "/", TRANSLATED_WORDS);
+
 pub fn load_translated_words() -> Result<HashMap<String, String>, Box<dyn Error>> {
     initialize_directory()?;
-    let file =
-        File::open("scrambler_data/translated_words.json").map_err(|inner| LoadFileError {
-            name: "scrambler_data/translated_words.json",
-            source: inner,
-        })?;
+    let file = File::open(TRANSLATED_WORDS_FILE).map_err(|inner| LoadFileError {
+        name: TRANSLATED_WORDS_FILE,
+        source: inner,
+    })?;
 
     let reader = BufReader::new(file);
     let result = serde_json::from_reader(reader).map_err(|inner| LoadFileError {
-        name: "scrambler_data/translated_words.json",
+        name: TRANSLATED_WORDS_FILE,
         source: inner,
     })?;
     Ok(result)
@@ -29,15 +33,15 @@ pub fn save_translated_words(words: &HashMap<String, String>) -> Result<(), Box<
         .write(true)
         .create(true)
         .truncate(true)
-        .open("scrambler_data/translated_words.json")
+        .open(TRANSLATED_WORDS_FILE)
         .map_err(|inner| SaveFileError {
-            name: "scrambler_data/translated_words.json",
+            name: TRANSLATED_WORDS_FILE,
             source: inner,
         })?;
 
     let writer = BufWriter::new(file);
     serde_json::to_writer(writer, words).map_err(|inner| SaveFileError {
-        name: "scrambler_data/translated_words.json",
+        name: TRANSLATED_WORDS_FILE,
         source: inner,
     })?;
 
@@ -45,8 +49,8 @@ pub fn save_translated_words(words: &HashMap<String, String>) -> Result<(), Box<
 }
 
 fn initialize_directory() -> Result<(), Box<dyn Error>> {
-    fs::create_dir_all("scrambler_data").map_err(|inner| CreateDirectoryError {
-        name: "scrambler_data",
+    fs::create_dir_all(DATA_DIRECTORY).map_err(|inner| CreateDirectoryError {
+        name: DATA_DIRECTORY,
         source: inner,
     })?;
     Ok(())
