f42da0eb6b5d19c7efd36ebc1222b6c3b7fe268a
Nick <aelterman.nick@gmail.com>
2023-06-06 00:01:18 +0200

    Add proper translation generation
    When a new translation is generated, it creates a word of a random size
    clamped between half the original size and double the original size.
    The characters of the words are uniformly chosen from the alphabet.
    Translations that have the same character trice in a row are discarded
    and a new translation is created.

--- src/scrambler/generator.rs
+++ src/scrambler/generator.rs
@@ -1,7 +1,9 @@
-use std::cmp::min;
 use std::error::Error;
 
+use itertools::Itertools;
 use log::error;
+use rand::Rng;
+use unicode_segmentation::UnicodeSegmentation;
 
 use crate::scrambler::storage;
 
@@ -19,17 +21,66 @@ pub fn new_translation(word: &str) -> Result<Translation, Box<dyn Error>> {
 
     let alphabet = storage::load_alphabet()?;
 
-    error!("Using dummy translation generation by simply replacing the first three letters with the alphabet. Proper generation is not implemented yet.");
-    let mut result = word.to_owned();
-    let range = min(result.len(), alphabet.len());
-    result.replace_range(
-        0..range,
-        &alphabet
-            .iter()
-            .map(|glyph| glyph.symbol.as_str())
-            .collect::<Vec<&str>>()
-            .join("")[0..range],
-    );
+    let original_length = word.graphemes(true).count();
+    let mut result = create_random_word(&alphabet, original_length);
+    while !is_valid_word(&result) {
+        result = create_random_word(&alphabet, original_length)
+    }
 
     Ok(Translation::new(result))
+}
+
+fn is_valid_word(word: &str) -> bool {
+    let graphemes = word.graphemes(true);
+    for (a, b, c) in graphemes.tuple_windows() {
+        if a == b && b == c {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_valid_word;
+
+    #[test]
+    fn triple_char_is_not_valid_word() {
+        assert!(!is_valid_word("aaa"))
+    }
+
+    #[test]
+    fn double_char_is_valid_word() {
+        assert!(is_valid_word("aa"))
+    }
+
+    #[test]
+    fn triple_char_with_other_char_in_between_is_valid_word() {
+        assert!(is_valid_word("aabaa"))
+    }
+}
+
+fn create_random_word(alphabet: &Vec<Glyph>, original_length: usize) -> String {
+    let mut random_generator = rand::thread_rng();
+
+    let (min_length, max_length) = calculate_new_length(original_length);
+    let length = random_generator.gen_range(min_length..=max_length);
+
+    let mut result = String::with_capacity(length);
+    for _ in 0..length {
+        let random_glyph = &alphabet[random_generator.gen_range(0..alphabet.len())];
+        result.push_str(&random_glyph.symbol);
+    }
+
+    result
+}
+
+fn calculate_new_length(original_length: usize) -> (usize, usize) {
+    let mut min_length = original_length / 2;
+    if min_length == 0 {
+        min_length = 1;
+    }
+    let max_length = original_length * 2;
+
+    (min_length, max_length)
 }
